49c6c718fe0315223189bf6e0e47f9a9a17fb517
Kirill Kotikov <liamka@me.com>
2022-10-24 18:53:01 +0300

    Delete 2222 directory

--- 2222/README.md
+++
@@ -1,5 +0,0 @@
-# 2222
-
-### Statistics:
-- **Total items:** 4134
-- **Last updated:** 2022-10-24 10:56:36
--- 2222/integrations/Java/OkHttp.java
+++
@@ -1,9 +0,0 @@
-OkHttpClient client = new OkHttpClient();
-
-Request request = new Request.Builder()
-	.url("https://api_dev.apitube.io/v1/sets/46796339?limit=250&offset=0")
-	.get()
-	.addHeader("X-ApiTube-Key", "***KEY***")
-	.build();
-
-Response response = client.newCall(request).execute();
