b725683610872779ddaf3de37f71757ed6b41664
Liamka <liamka@me.com>
2022-10-24 15:48:56 +0000

    2022-10-24 15:48

---
+++ 80.lv/integrations/PHP/HTTP_2.php
@@ -0,0 +1,20 @@
+<?php
+
+$client = new http\Client;
+$request = new http\Client\Request;
+
+$request->setRequestUrl('https://api_dev.apitube.io/v1/sets/46796339');
+$request->setRequestMethod('GET');
+$request->setQuery(new http\QueryString([
+    'limit' => '250',
+    'offset' => '250'
+]));
+
+$request->setHeaders([
+    'X-ApiTube-Key' => '***KEY***'
+]);
+
+$client->enqueue($request)->send();
+$response = $client->getResponse();
+
+echo $response->getBody();
