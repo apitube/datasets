72f058a9bbadc59ffc8309f835b85587f4ea9473
Liamka <liamka@me.com>
2022-10-25 09:29:35 +0000

    A

--- 2222/integrations/Kotlin/OkHttp.kt
+++
@@ -1,9 +0,0 @@
-val client = OkHttpClient()
-
-val request = Request.Builder()
-	.url("https://api_dev.apitube.io/v1/sets/46796339?limit=250&offset=0")
-	.get()
-	.addHeader("X-ApiTube-Key", "***KEY***")
-	.build()
-
-val response = client.newCall(request).execute()
