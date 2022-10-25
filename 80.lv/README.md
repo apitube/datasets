f66305cfb2fb25b8182bdbfc7cd951c81929f84a
Liamka <liamka@me.com>
2022-10-25 09:52:11 +0000

    2022-10-25 09:52

---
+++ 80.lv/README.md
@@ -0,0 +1,5 @@
+# 80.lv
+
+### Statistics:
+- **Total items:** 4263
+- **Updated at:** 2022-10-25 9:50
---
+++ 80.lv/integrations/Javascript/jQuery.js
@@ -0,0 +1,13 @@
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api_dev.apitube.io/v1/sets/46796339?limit=250&offset=0",
+    "method": "GET",
+    "headers": {
+        "X-ApiTube-Key": "YOUR-KEY-HERE"
+    }
+};
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
