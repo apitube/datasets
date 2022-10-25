be806d61fa43771bb02d00ce6276e081e791bdf3
Liamka <liamka@me.com>
2022-10-25 09:53:33 +0000

    2022-10-25 09:53

--- 80.lv/README.md
+++
@@ -1,5 +0,0 @@
-# 80.lv
-
-### Statistics:
-- **Total items:** 4263
-- **Updated at:** 2022-10-25 9:50
--- 80.lv/integrations/Java/OkHttp.java
+++
@@ -1,9 +0,0 @@
-OkHttpClient client = new OkHttpClient();
-
-Request request = new Request.Builder()
-	.url("https://api_dev.apitube.io/v1/sets/46796339?limit=250&offset=0")
-	.get()
-	.addHeader("X-ApiTube-Key", "YOUR-KEY-HERE")
-	.build();
-
-Response response = client.newCall(request).execute();
