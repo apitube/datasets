a2184e412b90979491b5a7bebeb14c08ffdc0d32
Liamka <liamka@me.com>
2022-10-25 09:50:03 +0000

    2022-10-25 09:50

--- 80.lv/integrations/C#/Unirest.cs
+++
@@ -1,4 +0,0 @@
-var client = new RestClient("https://api_dev.apitube.io/v1/sets/46796339?limit=250&offset=0");
-var request = new RestRequest(Method.GET);
-request.AddHeader("X-ApiTube-Key", "YOUR-KEY-HERE");
-IRestResponse response = client.Execute(request);
--- 80.lv/integrations/Powershell/Invoke-WebRequest.ps1
+++
@@ -1,3 +0,0 @@
-$headers=@{}
-$headers.Add("X-ApiTube-Key", "YOUR-KEY-HERE")
-$response = Invoke-WebRequest -Uri 'https://api_dev.apitube.io/v1/sets/46796339?limit=250&offset=0' -Method GET -Headers $headers
