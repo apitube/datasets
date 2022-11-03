bd28bf1ae13b0478da6c2f941ebd52998779be8d
Liamka <liamka@me.com>
2022-11-03 09:49:48 +0000

    2022-11-03T09:49:48+00:00

---
+++ 80.lv/integrations/Node.js/Unirest.js
@@ -0,0 +1,19 @@
+const unirest = require("domains/api/storage/app/API/v1/Export/Stubs/integrations/Node.js/Unirest");
+
+const req = unirest("GET", "https://api_dev.apitube.io/v1/sets/30054214");
+
+req.query({
+    "limit": "250",
+    "offset": "0",
+});
+
+req.headers({
+    "X-ApiTube-Key": "YOUR-KEY-HERE",
+    "useQueryString": true
+});
+
+req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+
+    console.log(res.body);
+});
